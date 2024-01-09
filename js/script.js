fbead564efce6cfaed6ea86f8f6b678cece3dbd0
Sung <sungjincho92@gmail.com>
2024-01-09 00:36:30 -0300

    minor fixes

--- js/script.js
+++ js/script.js
@@ -3,9 +3,16 @@ const inputLabel = document.querySelector('.js-label');
 
 const billInput = document.querySelector('.js-billInput');
 const peopleInput = document.querySelector('.js-peopleInput');
+const customInput = document.querySelector('.js-custom');
 
 const tipXPerson = document.querySelector('.js-tipxperson');
 const totalXPerson = document.querySelector('.js-totalxperson');
+
+const resetButton = document.querySelector('.btn-reset');
+
+
+
+/**********************Event Listeners***************************/
 
 inputsController.addEventListener('click', e => {
 
@@ -16,16 +23,38 @@ inputsController.addEventListener('click', e => {
         const childTip = e.target.parentElement.querySelector('.tip-percentage');
         const tipNum = parseInt(childTip.innerHTML.slice(0, -1), 10);
         calculateTotal(tipNum / 100);
+    } else if(targetType === 'number') {
+        console.log("hi");
+
+        // Uncheck all other inputs
+        clearAllInputs();
+
+        customInput.addEventListener('input', () => {
+            calculateTotal(parseInt(customInput.value, 10));
+        })
     }
 });
+
+resetButton.addEventListener('click', () => {
+    billInput.value = "";
+    peopleInput.value = "";
+    customInput.value = "";
+    tipXPerson.innerHTML = "$0.00";
+    totalXPerson.innerHTML = "$0.00";
+    clearAllInputs();
+});
+
+/*********************Functions*************************/
 
 const calculateTotal = function (num) {
     const peopleInputInt = parseInt(peopleInput.value, 10);
     const billInputInt = parseInt(billInput.value, 10);
 
     // It is NaN when there is no input in Number of People
-    if(isNaN(peopleInputInt)) {    
-        console.log("Number of People is empty");
+    if(isNaN(peopleInputInt) || isNaN(num)) {
+        console.log("Number of People is empty or Custom num is empty");
+        tipXPerson.innerHTML = "$0.00";
+        totalXPerson.innerHTML = "$0.00";
     } else { // There is an input in Number of People
         const totalTip = (billInputInt * num) / peopleInputInt;
         const totalPerson = totalTip + (billInputInt / peopleInputInt);
@@ -36,6 +65,15 @@ const calculateTotal = function (num) {
         totalXPerson.innerHTML = "$" + totalPerson.toFixed(2);
         setSuccess()
     }
+}
+
+// Uncheck all % Inputs
+function clearAllInputs() {
+    const allTipPercentage = inputsController.querySelectorAll('input');
+
+    allTipPercentage.forEach((el) => {
+        el.checked = false;
+    });
 }
 
 function setErrorFor(message) {
